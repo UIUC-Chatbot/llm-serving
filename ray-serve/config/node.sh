088f1576aa81d7d8d5cad89d404cad2b1733b271
zhourrr <zhouqinren@foxmail.com>
2024-04-03 22:28:57 -0500

    Update requirements.txt

--- ray-serve/config/node.sh
+++ ray-serve/config/node.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=ray-worker           # Job name
+#SBATCH --job-name=RayNode              # Job name
 #SBATCH --partition=a100                # Partition name
 #SBATCH --gres=gpu:2                    # Request GPU resource
 #SBATCH --ntasks=1                      # Run a single task
-#SBATCH --cpus-per-task=16               # Number of CPU cores per task
+#SBATCH --cpus-per-task=16              # Number of CPU cores per task
 #SBATCH --time=01:00:00                 # Time limit hrs:min:sec
 #SBATCH --output=ray-worker-%j.log      # Standard output and error log
 
